034316deecc4a2d700f38035a260b77422cde11a
Ivan Quetz <ivanquetz@gmail.com>
2018-08-03 00:57:08 +0500

    global styles, hot reload tested

--- public/App.vue
+++ public/App.vue
@@ -1,3 +1,19 @@
 <template lang="pug">
 router-view
 </template>
+
+<style lang="stylus">
+body {
+  font-family "Benton Sans", "Helvetica Neue", helvetica, arial, sans-serif
+  margin 0px
+  padding 0px
+}
+
+* {
+  box-sizing border-box
+}
+
+.ba
+  border 1px solid black
+
+</style>
